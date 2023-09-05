62771bc16b22a79b25fd49c8e839232a31d15c60
gusSato <gustavo.o.sato@gmail.com>
2023-09-05 11:11:12 -0300

    Criação do filtro de busca na tabela de serviços na página administrativa

--- src/main/java/com/soulcode/goserviceapp/repository/ServicoRepository.java
+++ src/main/java/com/soulcode/goserviceapp/repository/ServicoRepository.java
@@ -1,11 +1,13 @@
 package com.soulcode.goserviceapp.repository;
 
 import com.soulcode.goserviceapp.domain.Servico;
+import com.soulcode.goserviceapp.domain.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ServicoRepository extends JpaRepository<Servico, Long> {
@@ -18,4 +20,7 @@ public interface ServicoRepository extends JpaRepository<Servico, Long> {
             " JOIN usuarios u ON u.id = ps.prestador_id" +
             " WHERE u.email = ?", nativeQuery = true)
     List<Servico> findByPrestadorEmail(String email);
+
+    @Query(value = "SELECT * FROM servicos s WHERE s.nome LIKE %?1%", nativeQuery = true)
+    List<Servico> findServiceByNome(String search);
 }
